ad93d8ccc23bedb7b16c8c6daae8eb29e6132a9e
Nadja Brandys <nadiusza@gmail.com>
2020-07-13 17:05:05 +0200

    first commit

---
+++ client/app.js
@@ -0,0 +1,60 @@
+'use strict';
+{
+  
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
+let userName = '';
+
+  function login(e) {
+    e.preventDefault();
+    if (!userNameInput.value) {
+      alert('this field cannot be empty');
+      return;
+    } else {
+      userName = userNameInput.value;
+      loginForm.classList.remove('show');
+      messagesSection.classList.add('show');
+    }
+  }
+
+  function addMessage(author, content) {
+    const message = document.createElement('li');
+    message.classList.add('message');
+    message.classList.add('message--received');
+    if (author === userName) message.classList.add('message--self');
+    message.innerHTML = `
+      <h3 class="message__author">${userName === author ? 'You' : author}</h3>
+      <div class="message__content">
+        ${content}
+      </div>
+    `;
+    messagesList.appendChild(message);
+  }
+
+  function sendMessage(e){
+    e.preventDefault();
+  
+
+    if (!messageContentInput) {
+      alert('this field caanot be empty');
+      return;
+    } else {
+      addMessage(userName, messageContentInput.value);
+      messageContentInput.value = '';
+    }
+  }
+
+  loginForm.addEventListener('sumbit', e => {
+    login(e);
+  });
+
+  addMessageForm.addEventListener('submit', e => {
+    sendMessage()
+  });
+
+
+}
